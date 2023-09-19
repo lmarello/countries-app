2156ebb69484475c8f6bced868e000fd55eafa52
leomarello <leomarello@gmail.com>
2023-09-18 23:35:58 -0300

    feat: initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme') // eslint-disable-line
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#FFD200',
+				// color 111111 con 50% de opacidad
+				gray: 'rgba(17, 17, 17, 0.5)'
+			},
+			sans: {
+				fontFamily: ['Montserrat', ...defaultTheme.fontFamily.sans]
+			},
+			screens: {
+				medium: '468px',
+				mobile: '411px',
+				tablet: '721px',
+				laptop: '890px'
+			}
+		}
+	},
+	plugins: []
+}
